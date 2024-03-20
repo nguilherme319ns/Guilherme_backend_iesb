d266988d81afc83cf1f22250352cec56763dbc6c
nguilherme319ns <nguilherme319@gmail.com>
2024-03-20 19:49:17 -0300

    atualizando dados

--- 01-primeira-api/index.js
+++ 01-primeira-api/index.js
@@ -1,14 +1,24 @@
 // importando modulo do express
 const express = require('express')
 
-const app = express ()
- 
-app.get("/hello",(req ,res) =>{
-    res.send("nome bom ai !!!")
+// cria uma aplicação express
+const app = express()
 
+// construir a lógica (o contrato da minha api)
+app.get("/hello", (req, res) => {
+    res.send("Hello World!")
 })
 
-app.listen(3000, ()  => {
+app.get("/nome", (req, res) => {
+    res.send("João Paulo!")
+})
 
-   console.log("api iniciado! rondando em https://localhost:3000") 
+app.post("/teste", (req, res) => {
+    res.send("TESTE POST OK!")
+})
+
+// startando servidor(backend - api) para escutar comunicações
+// na porta 3000
+app.listen(3000, () => {
+    console.log("Api iniciando! Rodando em http://localhost:3000")
 })
